318e2a07ddce20508a4f6d9406dc96aeb15a73a6
kumailr7 <kumailrizvi70@gmail.com>
2024-08-05 20:09:36 +0530

    fix:(git_push.sh): Updated the code based
    Removed the old code base  in the git_push.sh and updated the code with branch

--- Commands/git_push.sh
+++ Commands/git_push.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-# # Function to push changes
-# push_changes() {
-#     echo " "
-#     # Ask for confirmation to push the changes
-#     if gum confirm "🚀 Ready to blast off and push the commit to the remote? 🌌"; then
-#         echo "$(gum style --italic --foreground 80 'Damn, enjoy your code going live! 🎉🚀')"
-#         echo " "
-        
-#         # Run git push and store the output
-#         push_output=$(git push 2>&1)
-
-#         # Vibrate the output with gum
-#         echo "$push_output" | gum style --border rounded --padding "1 2" --width 80 --margin "1" --foreground 82 --bold
-
-#     else
-#         echo "$(gum style --italic --foreground 70 'Alright, no space travel today. 🌠 Maybe next time! 🚀👨‍🚀')"
-#     fi
-# }
-
-#!/bin/bash
-
 push_changes() {
     echo " "
 
@@ -47,6 +26,6 @@ push_changes() {
         # Display push output
         echo "$push_output" | gum style --border rounded --padding "1 2" --width 80 --margin "1" --foreground 82 --bold
     else
-        echo "$(gum style --italic --foreground 159 '🛑 Operation canceled. No code will be pushed today. 🌌')"
+        echo "$(gum style --italic --foreground 159 'Alright, no space travel today. 🌠 Maybe next time! 🚀👨‍🚀')"
     fi
 }
