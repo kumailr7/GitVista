374fae9db77412f06f58f7c4e64a7bfdf59f4105
kumailr7 <kumailrizvi70@gmail.com>
2024-08-06 03:04:05 +0530

    fix:(git_intro.sh):

--- git_intro.sh
+++ git_intro.sh
@@ -107,19 +107,21 @@ git_vista() {
 handle_keypress() {
     local key
     echo -n "Press 'm' to open the Git menu or 'q' to quit: "
-    read -n 1 key
-    case $key in
-        m|M)
-            git_vista
-            ;;
-        q|Q)
-            echo "$(gum style --foreground 160 --bold '👋 Quitting...')"
-            exit 0
-            ;;
-        *)
-            echo "$(gum style --foreground 160 --bold '❓ Invalid key. Press "m" to open the menu or "q" to quit.')"
-            ;;
-    esac
+    while true; do
+        read -n 1 key
+        case $key in
+            m|M)
+                git_vista
+                ;;
+            q|Q)
+                echo "$(gum style --foreground 160 --bold '👋 Quitting...')"
+                exit 0
+                ;;
+            *)
+                echo "$(gum style --foreground 160 --bold '❓ Invalid key. Press "m" to open the menu or "q" to quit.')"
+                ;;
+        esac
+    done
 }
 
 # Main workflow
